5ad72dfd961df4569eaaf29dd7a380bc594427b4
Stas Demchuk <standemchuk@gmail.com>
2017-03-17 15:48:47 +0200

    getting rid of static variables

--- index.jsx
+++ index.jsx
@@ -72,7 +72,7 @@ export default class CountUp extends Component {
     const { className, start, style } = this.props;
 
     return (
-      <span className={className} style={style} ref={this.refSpan}>
+      <span className={className} style={style}>
         {start}
       </span>
     );
